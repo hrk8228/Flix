905a571aa19094b7390931f8ab71574b862b2964
hrk8228 <kumarharini21@gmail.com>
2022-09-20 02:05:09 -0500

    week 2 updates

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Harini Kumar on 9/20/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
